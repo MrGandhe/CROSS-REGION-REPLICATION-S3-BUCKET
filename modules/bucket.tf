a0c12604d0c276f66cca36e9b3fc4c0b220c7aca
MrGandhe <100147587+MrGandhe@users.noreply.github.com>
2022-09-30 18:14:26 +0530

    Update bucket.tf

--- modules/bucket.tf
+++ modules/bucket.tf
@@ -5,6 +5,13 @@ resource "aws_s3_bucket" "destination" {
 resource "aws_s3_bucket_versioning" "destination" {
   bucket = aws_s3_bucket.destination.id
   versioning_configuration {
-    status = "Enabled"
+    status = var.s3_bucket_versioning_status
+  }
+}
+
+resource "aws_s3_bucket_versioning" "source" {
+  bucket = var.source_bucket_name
+  versioning_configuration {
+    status = var.s3_bucket_versioning_status
   }
 }
