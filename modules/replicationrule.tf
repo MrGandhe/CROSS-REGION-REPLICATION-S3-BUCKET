ac257a7af64203edeca0f52d84364fcd9a84c83e
MrGandhe <100147587+MrGandhe@users.noreply.github.com>
2022-10-03 13:31:43 +0530

    Update replicationrule.tf

--- modules/replicationrule.tf
+++ modules/replicationrule.tf
@@ -1,6 +1,8 @@
 resource "aws_s3_bucket_replication_configuration" "replication" {
   # Must have bucket versioning enabled first
-
+  depends_on = [
+    aws_s3_bucket_versioning.source
+  ]
   role   = aws_iam_role.replication.arn
   bucket = var.source_bucket_name
 
